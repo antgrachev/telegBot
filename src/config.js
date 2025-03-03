fa1660017b8cbcd67d0fc1ac7cbace90729b2af9
Antony <antgrachev2312@gmail.com>
2025-03-03 21:05:02 +0300

    add string session

--- src/config.js
+++ src/config.js
@@ -5,6 +5,16 @@ export const BOT_TOKEN = process.env.BOT_TOKEN;
 export const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
 export const WEBHOOK_URL = `https://telegbot-qgzu.onrender.com/webhook/${BOT_TOKEN}`;
 
+export const API_ID = Number(process.env.API_ID);  // Преобразуем в число
+export const API_HASH = process.env.API_HASH;
+export const SESSION_STRING = process.env.SESSION_STRING;
+
+if (!BOT_TOKEN || !OPENAI_API_KEY || !API_ID || !API_HASH || !SESSION_STRING) {
+    console.error('❌ ERROR: Убедись, что все переменные окружения указаны в .env');
+    process.exit(1);
+}
+
+
 if (!BOT_TOKEN || !OPENAI_API_KEY) {
     console.error('❌ ERROR: Убедись, что BOT_TOKEN и OPENAI_API_KEY указаны в .env');
     process.exit(1);
